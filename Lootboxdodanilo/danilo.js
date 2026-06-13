17ee4ee181a0faf60b59302c2103cee5e99581ce
Miguel Lucas Da Silva Farias <muguellucassilva@gmail.com>
2026-06-13 00:22:53 -0300

    mudando as fotos

--- Lootboxdodanilo/danilo.js
+++ Lootboxdodanilo/danilo.js
@@ -1,19 +1,123 @@
-contador = 1
+﻿contador = 1
 function star() {
    if (contador == 1) {
+      if (Math.random() < 0.30) { 
+    document.getElementById('imagem-dinamica').src = "super raro.webp"
+      
+          if (Math.random() < 0.25) { 
+          document.getElementById('imagem-dinamica').src = "Epic_Chaos_Drop.webp"
+
+              if (Math.random() < 0.15) {
+          document.getElementById('imagem-dinamica').src = "Mythic_Chaos_Drop.webp"
+
+                  if (Math.random() < 0.10) {
+    document.getElementById('imagem-dinamica').src = "Legendary_Chaos_Drop.webp"
+
+                      if (Math.random() < 0.05) {
+          document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          if (Math.random() < 0.02) {
+    document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          }
+                      }
+                  }
+              }
+          }
+      }
       contador++
       bolinhapreta4.className = "bolinhaspretas"
    }
    else if (contador == 2) {
+            if (Math.random() < 0.30) { 
+    document.getElementById('imagem-dinamica').src = "super raro.webp"
+      
+          if (Math.random() < 0.25) { 
+          document.getElementById('imagem-dinamica').src = "Epic_Chaos_Drop.webp"
+
+              if (Math.random() < 0.15) {
+          document.getElementById('imagem-dinamica').src = "Mythic_Chaos_Drop.webp"
+
+                  if (Math.random() < 0.10) {
+    document.getElementById('imagem-dinamica').src = "Legendary_Chaos_Drop.webp"
+
+                      if (Math.random() < 0.05) {
+          document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          if (Math.random() < 0.02) {
+    document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          }
+                      }
+                  }
+              }
+          }
+      }
+
       contador++
       bolinhapreta3.className = "bolinhaspretas"
    }
    else if (contador == 3) {
+            if (Math.random() < 0.30) { 
+    document.getElementById('imagem-dinamica').src = "super raro.webp"
+      
+          if (Math.random() < 0.25) { 
+          document.getElementById('imagem-dinamica').src = "Epic_Chaos_Drop.webp"
+
+              if (Math.random() < 0.15) {
+          document.getElementById('imagem-dinamica').src = "Mythic_Chaos_Drop.webp"
+
+                  if (Math.random() < 0.10) {
+    document.getElementById('imagem-dinamica').src = "Legendary_Chaos_Drop.webp"
+
+                      if (Math.random() < 0.05) {
+          document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          if (Math.random() < 0.02) {
+    document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          }
+                      }
+                  }
+              }
+          }
+      }
+
       contador++
       bolinhapreta2.className = "bolinhaspretas"
    }
    else if (contador == 4) {
+            if (Math.random() < 0.30) { 
+    document.getElementById('imagem-dinamica').src = "super raro.webp"
+      
+          if (Math.random() < 0.25) { 
+          document.getElementById('imagem-dinamica').src = "Epic_Chaos_Drop.webp"
+
+              if (Math.random() < 0.15) {
+          document.getElementById('imagem-dinamica').src = "Mythic_Chaos_Drop.webp"
+
+                  if (Math.random() < 0.10) {
+    document.getElementById('imagem-dinamica').src = "Legendary_Chaos_Drop.webp"
+
+                      if (Math.random() < 0.05) {
+          document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          if (Math.random() < 0.02) {
+    document.getElementById('imagem-dinamica').src = "Ultra_Chaos_Drop.webp"
+
+                          }
+                      }
+                  }
+              }
+          }
+      }
+
       contador = 0
-      bolinhapreta1.className = "bolinhaspretas"  
+      bolinhapreta1.className = "bolinhaspretas"
    }
+ let A =  console.log(Math.random())
+let raridade = "Raro";
+
+
+console.log(raridade);
 }
